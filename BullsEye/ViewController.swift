78a32d714a759d21a0d3d2922c28cb527b05cc89
Mário Silva <mjgs.biz@gmail.com>
2015-01-30 21:36:08 +0000

    Project Skeleton

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Mário Silva on 30/01/15.
+//  Copyright (c) 2015 Mário Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
