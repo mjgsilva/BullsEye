c1e67ddf02dfd1a24df64f905b390a2c7dab8009
Mário Silva <mjgs.biz@gmail.com>
2015-02-02 11:43:40 +0000

    About controller

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Mário Silva on 01/02/15.
+//  Copyright (c) 2015 Mário Silva. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func close() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+}
